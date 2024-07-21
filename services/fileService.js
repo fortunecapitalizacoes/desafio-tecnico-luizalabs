5b888956c9a4d6ef7e935bd1103ea529e0ea6d5a
csilva.ps <csilva.spassu@tbg.com.br>
2024-07-21 11:48:47 -0300

    parse de pedido para usuario

--- services/fileService.js
+++ services/fileService.js
@@ -3,19 +3,25 @@ const path = require('path');
 const PedidoDTO = require('../dtos/PedidoDTO');
 
 const processTextFile = (filePath) => {
+
+  const pedidos = [];
+
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         return reject(new Error('Failed to read the file'));
       }
-      // Dividir o conteúdo em linhas e imprimir
+
       const lines = data.split('\n');
-      lines.forEach(line => {
-        var teste = parseStringToPedido(line)
-        console.log(teste);
-        parseStringToPedido(line)
+
+      lines.forEach(linha => {
+        var pedido = parseStringToPedido(linha)
+        pedidos.push(pedido)
+      //  console.log(pedido);
       });
 
+      const usuarios = groupPedidosByUser(pedidos);
+      usuarios.forEach(usuario => console.log(usuario))
       resolve();
     });
   });
@@ -42,6 +48,56 @@ function parseStringToPedido(input) {
       dataCompra,
   };
 }
+
+const groupPedidosByUser = (pedidos) => {
+  const usersMap = new Map();
+
+  pedidos.forEach(pedido => {
+      // Cria uma instância de PedidoDTO
+      const pedidoDTO = new PedidoDTO(
+          pedido.idUsuario,
+          pedido.nome,
+          pedido.idPedido,
+          pedido.idProduto,
+          pedido.valorProduto,
+          pedido.dataCompra
+      );
+
+      if (!usersMap.has(pedidoDTO.idUsuario)) {
+          usersMap.set(pedidoDTO.idUsuario, {
+              user_id: pedidoDTO.idUsuario,
+              name: pedidoDTO.nome,
+              orders: [],
+          });
+      }
+
+      const user = usersMap.get(pedidoDTO.idUsuario);
+
+      let order = user.orders.find(o => o.order_id === pedidoDTO.idPedido);
+
+      if (!order) {
+          order = {
+              order_id: pedidoDTO.idPedido,
+              total: 0, // Total será calculado posteriormente
+              date: new Date(pedidoDTO.dataCompra),
+              products: [],
+          };
+          user.orders.push(order);
+      }
+
+      order.products.push({
+          product_id: pedidoDTO.idProduto,
+          value: pedidoDTO.valorProduto.toString(),
+      });
+
+      // Atualizando o total do pedido
+      order.total = (parseFloat(order.total) + pedidoDTO.valorProduto).toFixed(2).toString();
+  });
+
+  return Array.from(usersMap.values());
+};
+
+
 
 module.exports = {
   processTextFile,
